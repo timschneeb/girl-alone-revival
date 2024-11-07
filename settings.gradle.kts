b1a17b441419ba5e2e7dc82266e6d6007d9805b8
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-11-07 02:24:26 +0100

    Squashed 'patches/' content from commit 3e285e503
    git-subtree-dir: patches
    git-subtree-split: 3e285e503d858568d34a660d11d3c53b07bac74c

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "revanced-patches"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/revanced/registry")
+            credentials {
+                username = providers.gradleProperty("gpr.user").getOrElse(System.getenv("GITHUB_ACTOR"))
+                password = providers.gradleProperty("gpr.key").getOrElse(System.getenv("GITHUB_TOKEN"))
+            }
+        }
+    }
+}
+
+plugins {
+    id("app.revanced.patches") version "1.0.0-dev.6"
+}
+
+include(":patches:stub")
