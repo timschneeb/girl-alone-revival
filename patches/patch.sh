c3690e1b1110302f1ed1d6d9325eeb47402d3f68
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-11-08 03:48:54 +0100

    chore: add script

---
+++ patches/patch.sh
@@ -0,0 +1,1 @@
+java -jar ../revanced-cli/build/libs/revanced-cli-5.0.0-dev.10-all.jar patch -p patches/build/libs/patches-5.0.0-dev.1.rvp -e "Use a custom game server" -o girl-alone-revived.apk game.apk
