1b70f368c267778c8df09ec7b13a208ce0dfa3b1
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-11-08 03:48:33 +0100

    refactor: reduce user id length

--- patches/extensions/shared/src/main/java/app/revanced/extension/girlalone/UserIdPatch.java
+++ patches/extensions/shared/src/main/java/app/revanced/extension/girlalone/UserIdPatch.java
@@ -8,22 +8,13 @@ import java.util.Base64;
 
 public class UserIdPatch {
     public static void injectContext(Context context) {
-        Log.e("UserIdPatch", "Injecting context");
-        Log.e("UserIdPatch", "Context: " + context);
-
-
-        if(context == null) {
-            Log.e("UserIdPatch", "Context is null, cannot create UserId");
-            return;
-        }
-
         SharedPreferences sharedPref = context.getSharedPreferences("com.fleximind.girllivingalone.android.revival.v2.playerprefs", Context.MODE_PRIVATE);
         String existingUserId = sharedPref.getString("0", "");
         if(existingUserId.length() < 1) {
             String userId = "";
 
             SecureRandom random = new SecureRandom();
-            byte bytes[] = new byte[13];
+            byte bytes[] = new byte[11];
             random.nextBytes(bytes);
             Base64.Encoder encoder = Base64.getEncoder();
             userId = encoder.encodeToString(bytes).replace("=", "");
