219f1624636f6c70b2322e48b4ab7b837e74741b
Tim Schneeberger <tim.schneeberger@outlook.de>
2024-11-15 01:23:35 +0100

    fix: fall-back to brute-forcing the decryption key

--- GirlAloneServer.Core/Utils/AES.cs
+++ GirlAloneServer.Core/Utils/AES.cs
@@ -2,6 +2,7 @@
 
 using System.Security.Cryptography;
 using System.Text;
+#pragma warning disable SYSLIB0022
 
 namespace GirlAloneServer.Core.Utils;
 
@@ -28,13 +29,16 @@ public static class AES
         "masdfjklqeuio7890zvnasdfjk234u"
     ];
     
-    private static string GetIV()
+    private static string GetIV(int? index = null)
     {
+        if(index != null) 
+            return _ivList[index.Value % _ivList.Length];
+        
         var updatedTime = DateTimeOffset.UtcNow.AddSeconds(9875.0);
-        return _ivList[updatedTime.Hour % 7];
+        return _ivList[updatedTime.Hour % _ivList.Length];
     }
 
-    private static string GetKey()
+    private static string GetKey(int? index = null, int? timestampPrefix = null)
     {
         var serverTime = DateTimeOffset.UtcNow.AddSeconds(9875);
 
@@ -45,7 +49,12 @@ public static class AES
 
         var timestamp = (year + month + day + hour) * 0x85;
         
-        var key = _keyList[hour % 7];
+        if(index != null && timestampPrefix != null) 
+            // Only the first 2 characters of the timestamp are used.
+            // These are either 26 or 27 for the next few years.
+            return $"{_keyList[index.Value % _keyList.Length]}{timestampPrefix.Value}"[..32];
+        
+        var key = _keyList[hour % _keyList.Length];
         return $"{key}{timestamp}"[..32];
     }
     
@@ -58,16 +67,43 @@ public static class AES
     /// <returns>Decrypted UTF-8 string</returns>
     public static string DecryptCBC(string cipherTextBase64)
     {
-#pragma warning disable SYSLIB0022
         using var rijndael = new RijndaelManaged();
-#pragma warning restore SYSLIB0022
 
+        try
+        {
+            return DecryptCBC(rijndael, cipherTextBase64);
+        }
+        catch (Exception e)
+        {
+            // Fall back to brute-forcing the key and IV.
+            for (var i = 0; i < _keyList.Length; i++)
+            {
+                for (var j = 27; j >= 26; j--)
+                {
+                    try
+                    {
+                        return DecryptCBC(rijndael, cipherTextBase64, i, j);
+                    }
+                    catch
+                    {
+                        // Continue
+                    }
+                }
+            }
+
+            throw;
+        }
+    }
+    
+    private static string DecryptCBC(RijndaelManaged rijndael, string cipherTextBase64, 
+        int? index = null, int? timestampPrefix = null)
+    {
         rijndael.KeySize = 256;
         rijndael.BlockSize = 128;
         rijndael.Mode = CipherMode.CBC; 
         rijndael.Padding = PaddingMode.PKCS7;
-        rijndael.Key = Encoding.UTF8.GetBytes(GetKey());
-        rijndael.IV = Encoding.UTF8.GetBytes(GetIV());
+        rijndael.Key = Encoding.UTF8.GetBytes(GetKey(index));
+        rijndael.IV = Encoding.UTF8.GetBytes(GetIV(timestampPrefix));
 
         var cipherBytes = Convert.FromBase64String(cipherTextBase64);
 
